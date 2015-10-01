e28d8b14af9d5dabc672698c5f878fb95e8ae54c
Christopher Augg <chrisa@christopheraugg.com>
2015-10-01 12:43:19 -0700

    Initial commit

--- Fun Facts/FactBook.swift
+++ Fun Facts/FactBook.swift
@@ -18,7 +18,7 @@ struct FactBook {
         "It takes about 8 minutes for light from the Sun to reach Earth.",
         "Some bamboo plants can grow almost a meter in just one day.",
         "The state of Florida is bigger than England.",
-        "Some penguins can leap 2-3 meters out of the water.",
+        "Some penguins can leap 2 to 3 meters out of the water.",
         "On average, it takes 66 days to form a new habit.",
         "Mammoths still walked the earth when the Great Pyramid was being built." ]
     
